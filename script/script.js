29077f8571e96c13b357ae17e31e704fa3ce1351
Dariush Mirkarimi <71138545+DMirkarimi@users.noreply.github.com>
2021-04-15 14:57:38 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -8,3 +8,7 @@ window.addEventListener("scroll", function()
     let offset = window.pageYOffset;
     woop.style.backgroundPositionY = offset / 10 + "px";
     })
+
+for element of document.getElementsByClassName("textlink")
+{
+    element.style.setProperty("--before-width", element.length);}
