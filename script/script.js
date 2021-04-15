0f0f5de5f48fb7d31854968b80ed7826e4ee35a3
Dariush Mirkarimi <71138545+DMirkarimi@users.noreply.github.com>
2021-04-15 14:59:41 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,6 +9,5 @@ window.addEventListener("scroll", function()
     woop.style.backgroundPositionY = offset / 10 + "px";
     })
 
-for element of document.getElementsByClassName("textlink")
-{
+for (element of document.getElementsByClassName("textlink")){
     element.style.setProperty("--before-width", element.length);}
